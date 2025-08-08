c080dff9774c178957869fb91c3a1c25bc05b82a
ChrisG16 <christiang3750@outlook.com>
2025-08-07 20:54:53 -0500

    Updated script

--- data/DataProcessingScript.py
+++ data/DataProcessingScript.py
@@ -1,12 +1,9 @@
 import pandas as pd
 
-# Data files from the project in .csv
 files = ['daily_sales_data_0.csv', 'daily_sales_data_1.csv', 'daily_sales_data_2.csv']
 
-# Initialize an empty DataFrame to store results
 results = pd.DataFrame()
 
-# Iterate through each file, filter for 'pink morsel', and calculate sales
 for file in files:
     
     df = pd.read_csv(file)
@@ -23,5 +20,4 @@ for file in files:
 
 results.to_csv('filtered_results.csv', index=False)
 
-# Print the final results DataFrame for testing purposes
 print(results)
